3d815248f97edd284654fc92b837f2ac26bc1da2
tamnguyen93c <tamnguyen93c@gmail.com>
2021-12-16 07:15:27 +0700

    update

--- src/main/java/com/example/demo/interviews/ThreadPoolInterview.java
+++ src/main/java/com/example/demo/interviews/ThreadPoolInterview.java
@@ -1,19 +1,20 @@
-package com.example.demo.threads;
+package com.example.demo.interviews;
 
 import java.util.ArrayList;
-import java.util.concurrent.CountDownLatch;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class ThreadPoolInterview1 {
+public class ThreadPoolInterview {
 
 
-    // Tạo 50 thread mỗi thread loop 200K in ra 10 triệu;static int count = 0;
+    // Tạo 50 thread mỗi thread loop 200K in ra 10 triệu;static int count = 0; yêu cầu thread chạy song song
     static int count = 0;
 	public static void main(String[] args) throws InterruptedException {
 		ArrayList<Thread> list = new ArrayList<>();
 		for (int i = 0; i < 50; i++) {
 			Thread th = new Thread(new Runnable() {
 				@Override
-				public void run() {
+				public synchronized void run() {
 					for (int i = 0; i < 200000; i++) {
 						count++;
 					}
@@ -23,10 +24,10 @@ public class ThreadPoolInterview1 {
 			list.add(th);
 		}
         
-
 		// for (Thread t : list) {
         //     String name = t.getName();
         //     Thread.State state = t.getState();
+			
         //     int priority = t.getPriority();
         //     String type = t.isDaemon() ? "Daemon" : "Normal";
         //     System.out.printf("%-20s \t %s \t %d \t %s\n", name, state, priority, type);
@@ -35,7 +36,6 @@ public class ThreadPoolInterview1 {
 		// 	} catch (InterruptedException ex) {
 		// 	}
 		// }
-        
 		System.out.println("count: " + count);
 	}
 

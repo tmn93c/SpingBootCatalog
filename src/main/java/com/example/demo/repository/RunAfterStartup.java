87136646e2e218e9bfa8f6c692eeaa6af235511e
ndtam1 <ndtam@abc.com>
2023-06-14 22:39:26 +0700

    add rsakey example

--- src/main/java/com/example/demo/repository/RunAfterStartup.java
+++ src/main/java/com/example/demo/repository/RunAfterStartup.java
@@ -3,14 +3,24 @@ package com.example.demo.repository;
 import com.example.demo.mapper.TestMapper;
 import com.example.demo.model.TestEntity;
 import com.example.demo.model.UserModel;
+import com.example.demo.util.RsaKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyPair;
+import java.security.PublicKey;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -26,6 +36,15 @@ public class RunAfterStartup {
 
     private final TestMapper testMapper;
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Value("classpath:rsa.pub")
+    Resource resourcePub;
+
+    @Value("classpath:rsa.ppk")
+    Resource resourcePrivate;
+
 
     public RunAfterStartup(UserRepository userRepository, RoleRepository roleRepository, TestMapper testMapper) {
         this.userRepository = userRepository;
@@ -35,29 +54,38 @@ public class RunAfterStartup {
 
     @Transactional
     @EventListener(ApplicationReadyEvent.class)
-    public void insertWithQuery() {
+    public void insertWithQuery() throws Exception {
+        KeyPair keyPair = RsaKey.generateKey();
+        String message = "the answer to life the universe and everything";
+
+        String encode = RsaKey.encrypt(message, keyPair.getPublic());
+        String decode = RsaKey.decrypt(encode, keyPair.getPrivate());
+        File file = resourcePub.getFile();
+
+        String content = new String(Files.readAllBytes(file.toPath()));
+        PublicKey publicKey = RsaKey.getPubKey(file.toPath());
         Collection<TestEntity> testEntities = testMapper.findAll();
         TestEntity userModel = testMapper.getOneTest(1);
 
-        for (int i = 0; i < 5000; i++) {
-            try {
-                String newUsername = "tamnd" + i;
-                UserModel findUsername = userRepository.findByUsername(newUsername);
-                if (Objects.isNull(findUsername)) {
-                    entityManager.createNativeQuery(
-                                    "INSERT INTO user_model (account_expired, account_locked, credentials_expired, email, enabled, name, password, username) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);")
-                            .setParameter(1, false)
-                            .setParameter(2, false)
-                            .setParameter(3, false)
-                            .setParameter(4, "tamnd@gmail.com" + i)
-                            .setParameter(5, true)
-                            .setParameter(6, "NGUYEN DINH TAM")
-                            .setParameter(7, "{bcrypt}$2a$10$iyZmEzd13MQGF46fnbUzO.ItgMPo2NoVCnVBvYPWIbqSh2WRI4W5i")
-                            .setParameter(8, newUsername).executeUpdate();
-                }
-            } catch (NoResultException ex) {
-                System.out.println(ex);
-            }
-        }
+//        for (int i = 0; i < 5000; i++) {
+//            try {
+//                String newUsername = "tamnd" + i;
+//                UserModel findUsername = userRepository.findByUsername(newUsername);
+//                if (Objects.isNull(findUsername)) {
+//                    entityManager.createNativeQuery(
+//                                    "INSERT INTO user_model (account_expired, account_locked, credentials_expired, email, enabled, name, password, username) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?);")
+//                            .setParameter(1, false)
+//                            .setParameter(2, false)
+//                            .setParameter(3, false)
+//                            .setParameter(4, "tamnd@gmail.com" + i)
+//                            .setParameter(5, true)
+//                            .setParameter(6, "NGUYEN DINH TAM")
+//                            .setParameter(7, "{bcrypt}$2a$10$iyZmEzd13MQGF46fnbUzO.ItgMPo2NoVCnVBvYPWIbqSh2WRI4W5i")
+//                            .setParameter(8, newUsername).executeUpdate();
+//                }
+//            } catch (NoResultException ex) {
+//                System.out.println(ex);
+//            }
+//        }
     }
 }
---
+++ src/main/java/com/example/demo/util/RsaKey.java
@@ -0,0 +1,114 @@
+package com.example.demo.util;
+
+import javax.crypto.Cipher;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Random;
+import java.io.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class RsaKey {
+    private BigInteger n, e, d;
+
+    // Reading in RSA public key and private key
+    RsaKey(String input) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(input));
+            String line = in.readLine();
+            while (line!=null) {
+                if (line.indexOf("Modulus: ")>=0) {
+                    n = new BigInteger(line.substring(9));
+                }
+                if (line.indexOf("Public key: ")>=0) {
+                    e = new BigInteger(line.substring(12));
+                }
+                if (line.indexOf("Private key: ")>=0) {
+                    d = new BigInteger(line.substring(13));
+                }
+                line = in.readLine();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("--- Reading public key and private key ---");
+        System.out.println("Modulus: "+n);
+        System.out.println("Key size: "+n.bitLength());
+        System.out.println("Public key: "+e);
+        System.out.println("Private key: "+d);
+    }
+
+    // Testing encryption and description
+    public void test() {
+        Random rnd = new Random();
+        int size = rnd.nextInt(n.bitLength()-1);
+        BigInteger text = new BigInteger(size,rnd);
+        BigInteger cipher = text.modPow(e,n);
+        BigInteger decrypted = cipher.modPow(d,n);
+        boolean isPassed = text.equals(decrypted);
+        System.out.println("--- RSA encryption test ---");
+        System.out.println("Is test passed: "+isPassed);
+        System.out.println("Original text: "+text);
+        System.out.println("Cipher text: "+cipher);
+        System.out.println("Decrypted text: "+decrypted);
+    }
+
+    public static KeyPair generateKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+
+        KeyPair keyPair = kpg.generateKeyPair();
+
+        Key pub = keyPair.getPublic();
+        System.out.println("Public key format: " + pub.getFormat());
+
+        Key pvt = keyPair.getPrivate();
+        return keyPair;
+    }
+
+    public static void generatePublicKeyAndPrivateKey(KeyPair keypair, String outputFileWithoutExtension)
+            throws IOException {
+        OutputStream out = new FileOutputStream(outputFileWithoutExtension + ".key");
+        out.write(keypair.getPrivate().getEncoded());
+        out.close();
+
+        out = new FileOutputStream(outputFileWithoutExtension + ".pub");
+        out.write(keypair.getPublic().getEncoded());
+        out.close();
+    }
+
+
+    public static PublicKey getPubKey(Path filename)
+            throws Exception {
+
+        byte[] keyBytes = Files.readAllBytes(filename);
+
+        X509EncodedKeySpec spec =
+                new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePublic(spec);
+    }
+
+    public static String encrypt(String plainText, PublicKey publicKey) throws Exception {
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] cipherText = encryptCipher.doFinal(plainText.getBytes(UTF_8));
+
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static String decrypt(String cipherText, PrivateKey privateKey) throws Exception {
+        byte[] bytes = Base64.getDecoder().decode(cipherText);
+
+        Cipher decriptCipher = Cipher.getInstance("RSA");
+        decriptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return new String(decriptCipher.doFinal(bytes), UTF_8);
+    }
+}
